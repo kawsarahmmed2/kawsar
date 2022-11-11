f047961afec1cb80769058964a4a05d8c497263f
kawsarahmmed2 <kawsarahmmed17102@gmail.com>
2022-11-11 20:53:01 +0600

    extra commit

--- problem-solving.js
+++ problem-solving.js
@@ -50,15 +50,39 @@ console.log(totalCost);
 4th Problem = publicBusFare
 ----------------------*/
 
-function publicBusFare(busQuantity, ){
-    const busQuantity = 2 ;
-    const busCapacity = 50;
-    const busPassenger = busQuantity * busCapacity;
-    const microPassenger = 11;
-    const PublicBusTicketRate = 250;
-    
-    
 
-}
+     
+    function publicBusFare(total) {
+        const bus = 50
+        const micro = 11
+        const totalPassenger = total;
+        const tk = 250
+        if(50 <= total){
+            // console.log('run')
+            let  = total / 50
+            const abc = Math.floor()
+            let bus = total - 50 * abc 
+            if (11 <= bus) {
+                const car = bus / 11
+                const abc = Math.floor(car)
+                let cars = bus - 11 * abc 
+                const result = 250 * cars
+                console.log(result)
+                return result
+            }
+            else{
+                const result = 250 * bus
+                return result
+            }
+    
+        }
+     
+     
+        
+    }
+    
+    const ab = pu(55)
+    
+    console.log(ab)
 
-const population = publicBusFare(100);
+
